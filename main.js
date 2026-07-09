0606c61981420714cbe942820df840118e2ceb86
vanessa2103206 <vanessac2016@libero.it>
2026-07-09 11:16:36 +0200

    Codice separato con successo in main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+// 1. Dichiaro 5 variabili contenenti un numero ciascuna
+let num1 = 10;
+let num2 = 22;
+let num3 = 35;
+let num4 = 47;
+let num5 = 56;
+
+// 2. Calcolo la somma tra i 5 numeri
+let somma = num1 + num2 + num3 + num4 + num5;
+
+// 3. Calcolo la media
+let media = somma / 5;
+
+// 4. Mostro in console la frase esatta richiesta dalla traccia
+console.log("La somma tra i numeri equivale a " + somma + " e la media equivale a " + media);
